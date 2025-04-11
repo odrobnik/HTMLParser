232c2ea60deaaae236b4078955e605c1b52ab679
Oliver Drobnik <oliver@cocoanetics.com>
2025-04-11 11:26:02 +0200

    Try enum stuff for Linux

--- Sources/CHTMLParser/include/HTMLParserOptions.h
+++ Sources/CHTMLParser/include/HTMLParserOptions.h
@@ -11,17 +11,30 @@
 #ifndef HTMLParserOptions_h
 #define HTMLParserOptions_h
 
-#import <Foundation/Foundation.h>
-#include "libxml2-wrapper.h"
-
-typedef NS_OPTIONS(int32_t, HTMLParserOptions) {
-	HTMLParserOptionRecover       = 1 << 0,
-	HTMLParserOptionNoError       = 1 << 1,
-	HTMLParserOptionNoWarning     = 1 << 2,
-	HTMLParserOptionPedantic      = 1 << 3,
-	HTMLParserOptionNoBlanks      = 1 << 4,
-	HTMLParserOptionNoNet         = 1 << 5,
-	HTMLParserOptionCompact       = 1 << 6
-};
+#if defined(__OBJC__) && __has_feature(objc_modules)
+  // Objective-C + modules = can use Foundation
+  #import <Foundation/Foundation.h>
+  typedef NS_OPTIONS(int32_t, HTMLParserOptions) {
+	  HTMLParserOptionRecover   = 1 << 0,
+	  HTMLParserOptionNoError   = 1 << 1,
+	  HTMLParserOptionNoWarning = 1 << 2,
+	  HTMLParserOptionPedantic  = 1 << 3,
+	  HTMLParserOptionNoBlanks  = 1 << 4,
+	  HTMLParserOptionNoNet     = 1 << 5,
+	  HTMLParserOptionCompact   = 1 << 6
+  };
+#else
+  // Pure C / Linux-compatible version
+  typedef int32_t HTMLParserOptions;
+  enum {
+	  HTMLParserOptionRecover   = 1 << 0,
+	  HTMLParserOptionNoError   = 1 << 1,
+	  HTMLParserOptionNoWarning = 1 << 2,
+	  HTMLParserOptionPedantic  = 1 << 3,
+	  HTMLParserOptionNoBlanks  = 1 << 4,
+	  HTMLParserOptionNoNet     = 1 << 5,
+	  HTMLParserOptionCompact   = 1 << 6
+  };
+#endif
 
 #endif /* HTMLParserOptions_h */
